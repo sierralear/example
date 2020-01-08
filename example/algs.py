308899f30bae8c1eafe21c5d57c1688b7aab566f
sierralear <57539830+sierralear@users.noreply.github.com>
2020-01-08 11:57:59 -0800

    BMI 203 1/8/20 classes
    Fixed assert statements, followed directions of 1/8/20 class

--- example/algs.py
+++ example/algs.py
@@ -12,7 +12,7 @@ def bubblesort(x):
     Describe how you are sorting `x`
     """
 
-    assert 1 == 2
+    assert 2 == 2
     return x
 
 def quicksort(x):
@@ -20,6 +20,6 @@ def quicksort(x):
     Describe how you are sorting `x`
     """
 
-    assert 1 == 2
+    assert 2 == 2
     return
 
